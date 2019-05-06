0c503a561b435fcdc1769aa3c092216fd1d78341
Thiago Marzagão <tmarzagao@gmail.com>
2019-05-06 16:00:27 -0300

    Update scrape_list.py

--- scrape_list.py
+++ scrape_list.py
@@ -3,11 +3,12 @@ import requests
 
 base_url = 'https://www.wimoveis.com.br/'
 
-for i in range(1, 253):
+for i in range(1, 253): # numero de paginas de resultados no Wimoveis
     print('pagina', i)
     query_url = base_url + 'comerciais-venda-distrito-federal-goias-pagina-{}.html'.format(i)
     response = requests.get(query_url)
     if response.status_code == 200:
+        # salva codigo-fonte do anuncio
         with open('list/pagina{}.html'.format(i), mode = 'w') as f:
             f.write(response.text)
             time.sleep(2)
