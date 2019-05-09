8b55776f2c4afa608abee3e0a1fa21d13dc2599c
Thiago Marzagão <tmarzagao@gmail.com>
2019-05-09 10:57:50 -0300

    Update avaliacao.py

--- avaliacao.py
+++ avaliacao.py
@@ -6,8 +6,11 @@ from xgboost import XGBRegressor
 from sklearn.svm import SVR
 from sklearn.utils import shuffle
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
 from sklearn.model_selection import cross_val_predict
+
+# fixa seed (p/ permitir replicacao)
+random_state = 42
 
 # carrega dados
 df = pd.read_csv('dataset.csv')
@@ -19,6 +22,10 @@ df = df[['preco_total', 'area_util', 'local', 'vagas']]
 df = df[df['area_util'] < 50000]
 df = df[df['preco_total'] < 70000000]
 
+# cria amostra ficticia (imovel que queremos preficicar)
+x_new = pd.DataFrame({'preco_total': [1.0], 'area_util': [75], 'local': ['Noroeste'], 'vagas': [2]})
+df = df.append(x_new)
+
 # lineariza relacao entre preco e area util
 df['preco_total'] = df['preco_total'].map(lambda x: math.log(x))
 df['area_util'] = df['area_util'].map(lambda x: math.log(x))
@@ -26,6 +33,7 @@ df['area_util'] = df['area_util'].map(lambda x: math.log(x))
 # checa se relacoes fazem sentido
 #df.plot.scatter(x = 'area_util', y = 'preco_total')
 #df.plot.scatter(x = 'vagas', y = 'preco_total')
+#plt.show()
 
 # recodifica local
 new_locals = {
@@ -86,6 +94,8 @@ new_locals = {
     'Setor de Hotéis e Turismo Norte': 'asa_norte'
     }
 df['local'] = df['local'].map(lambda x: new_locals[x])
+
+# checa distribuicao dos imoveis por regiao
 print(df['local'].value_counts())
 
 # cria dummies p/ locais
@@ -94,24 +104,30 @@ for col in locais.columns:
     df[col] = locais[col]
 del df['local']
 
+# extrai row c/ amostra ficticia
+x_new = df[df['preco_total'] == 0]
+del x_new['preco_total']
+x_new = x_new.values
+df = df[df['preco_total'] > 0]
+
 # separa X e Y
 y = df['preco_total'].values
 del df['preco_total']
 X = df.values
 
 # aleatoriza ordem das amostras
-X, y = shuffle(X, y, random_state = 42)
+X, y = shuffle(X, y, random_state = random_state)
 
 # instancia modelo
-#clf = RandomForestRegressor(n_estimators = 1000, n_jobs = -1)
-#clf = SVR(kernel = 'poly', gamma = 'scale', epsilon = 0.001, C = 25)
-#clf = LinearRegression()
-#clf = AdaBoostRegressor(LinearRegression(), loss = 'exponential', n_estimators = 500)
-#clf = GradientBoostingRegressor()
-clf = XGBRegressor(n_estimators = 1000, booster = 'gbtree', reg_lambda = 0.5) # melhor desempenho
+#model = RandomForestRegressor(n_estimators = 1000, n_jobs = -1)
+#model = SVR(kernel = 'poly', gamma = 'scale', epsilon = 0.001, C = 25)
+#model = LinearRegression()
+#model = AdaBoostRegressor(LinearRegression(), loss = 'exponential', n_estimators = 500)
+model = GradientBoostingRegressor(loss = 'quantile', alpha = 0.5, n_estimators = 1000, random_state = random_state)
+#model = XGBRegressor(n_estimators = 1000, booster = 'gbtree', reg_lambda = 0.5) # melhor desempenho
 
 # treina modelo e gera estimativas
-yhat = cross_val_predict(clf, X, y, cv = 10)
+yhat = cross_val_predict(model, X, y, cv = 10)
 
 # poe precos estimados na escala original (R$)
 yhat_reais = np.exp(yhat)
@@ -136,6 +152,7 @@ print('erro relativo mediano:', erro_relativo_mediano)
 # plota algumas relacoes
 df = pd.DataFrame([])
 df['area_util'] = X[:,0]
+df['area_util'] = df['area_util'].map(lambda x: math.exp(x))
 df['yhat'] = yhat
 df['yhat_reais'] = yhat_reais
 df['y_reais'] = y_reais
@@ -146,21 +163,16 @@ df['erro_relativo'] = erros_relativos
 #df.plot.scatter(x = 'area_util', y = 'erro')
 #plt.show()
 
-# poe area util na escala original (m2)
-df['area_util'] = df['area_util'].map(lambda x: math.exp(x))
+# cria modelos p/ gerar estimativas e intervalos
+model_lower = GradientBoostingRegressor(loss = 'quantile', alpha = 0.25, n_estimators = 1000, random_state = random_state)
+model_mid = GradientBoostingRegressor(loss = 'quantile', alpha = 0.5, n_estimators = 1000, random_state = random_state)
+model_upper = GradientBoostingRegressor(loss = 'quantile', alpha = 0.75, n_estimators = 1000, random_state = random_state)
+model_lower.fit(X, y)
+model_mid.fit(X, y)
+model_upper.fit(X, y)
 
-# checa erro absoluto mediano por faixa de metragem
-step = 1000
-i = 0
-while i < (10 * step):
-    min_area, max_area = i, i + step
-    subset = df[(df['area_util'] >= min_area) & (df['area_util'] < max_area)]
-    erro_absoluto_mediano = subset['erro'].map(lambda x: abs(x)).median()
-    qtde = subset.shape[0]
-    preco_mediano = subset['y_reais'].median()
-    i += step
-    print(' ')
-    print('faixa:', min_area, 'm2 a', max_area, 'm2')
-    print('erro absoluto mediano:', round(erro_absoluto_mediano, 2))
-    print('qtde:', qtde)
-    print('preco mediano:', round(preco_mediano, 2))
+# estima valores p/ nova amostra
+yhat_lower = model_lower.predict(x_new)
+yhat_mid = model_mid.predict(x_new)
+yhat_upper = model_upper.predict(x_new)
+print(np.exp(yhat_lower), np.exp(yhat_mid), np.exp(yhat_upper))
